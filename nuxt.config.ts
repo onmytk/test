470167e57c3f46b61547ea1e540817170c32d599
onmytk <onmytk@gmail.com>
2023-07-24 02:01:06 +0900

    change vuetify to tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,23 +1,12 @@
 import { defineNuxtConfig } from 'nuxt/config'
-import vuetify from 'vite-plugin-vuetify'
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  build: {
-    transpile: [/vuetify/],
-  },
-  hooks: {
-    'vite:extendConfig': (config) => {
-      config.plugins!.push(vuetify())
+  css: ['@/assets/css/main.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
     },
   },
-  vite: {
-    ssr: {
-      noExternal: [/vuetify/],
-    },
-    define: {
-      'process.env.DEBUG': false,
-    },
-  },
-  css: ['@/assets/main.scss'],
-})
+});
--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,8 @@
 </script>
 
 <template>
-  <h1>Main Page</h1>
-  <v-btn>Test</v-btn>
+  <h1 class="text-3xl font-bold underline">Index.vue</h1>
+  <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+    Button
+  </button>
 </template>
