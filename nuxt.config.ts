ee4a495653573c55f67a85d71922e482d783ab98
onmytk <onmytk@gmail.com>
2023-07-24 02:20:51 +0900

    add cdnURL

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,8 @@ import { defineNuxtConfig } from 'nuxt/config'
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   app: {
-    baseURL: '/test/'
+    baseURL: '/test/',
+    cdnURL: '/test/'
   },
   ssr: false,
   css: ['@/assets/css/main.css'],
