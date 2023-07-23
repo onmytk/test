15258abbf2c8b5efd598a9104ed12dd69e498341
onmytk <onmytk@gmail.com>
2023-07-24 02:50:20 +0900

    docs?

---
+++ docs/_nuxt/index.21cdf10b.js
@@ -0,0 +1,1 @@
+import{_ as t,l as n,k as o,F as s,m as e}from"./entry.4007cf52.js";const c={},r=e("h1",{class:"text-3xl font-bold underline"},"Index.vue",-1),a=e("button",{class:"bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"}," Button ",-1);function l(d,_){return o(),n(s,null,[r,a],64)}const x=t(c,[["render",l]]);export{x as default};
---
+++ docs/_nuxt/test.a1d6e077.js
@@ -0,0 +1,1 @@
+import{_ as e,l as t,k as c}from"./entry.4007cf52.js";const n={};function r(s,o){return c(),t("h1",null,"Test Page")}const _=e(n,[["render",r]]);export{_ as default};
